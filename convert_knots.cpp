8e27600c1df184d26c0d256314771a7d1f149a42
alemg11 <156471064+alemg11@users.noreply.github.com>
2024-01-15 17:09:00 -0800

    Create convert_knots.cpp

---
+++ convert_knots.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() 
+{
+  int knots = 0;
+  cout << "Enter integer speed in knots: ";
+  cin >> knots;
+  cout << "Speed in miles per minute is "
+       << knots_to_miles_per_minute(knots)
+       << endl;
+  return 0;
+}
